d034e215cd9a99dc4952729773ff95af7407652b
weintraubb <laruweinconti@gmail.com>
2021-11-20 17:19:21 -0300

    TP4

---
+++ TP4/Debug/src/obj/Debug/testing/src/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../src/obj/Debug/testing/src/Employee.o \
+../src/obj/Debug/testing/src/addNodeTest.o \
+../src/obj/Debug/testing/src/addTest.o \
+../src/obj/Debug/testing/src/clearTest.o \
+../src/obj/Debug/testing/src/cloneTest.o \
+../src/obj/Debug/testing/src/containsAllTest.o \
+../src/obj/Debug/testing/src/containsTest.o \
+../src/obj/Debug/testing/src/deleteLinkedListTest.o \
+../src/obj/Debug/testing/src/getNodeTest.o \
+../src/obj/Debug/testing/src/getTest.o \
+../src/obj/Debug/testing/src/indexOfTest.o \
+../src/obj/Debug/testing/src/isEmptyTest.o \
+../src/obj/Debug/testing/src/lenTest.o \
+../src/obj/Debug/testing/src/main_test.o \
+../src/obj/Debug/testing/src/newTest.o \
+../src/obj/Debug/testing/src/popTest.o \
+../src/obj/Debug/testing/src/pushTest.o \
+../src/obj/Debug/testing/src/removeTest.o \
+../src/obj/Debug/testing/src/setTest.o \
+../src/obj/Debug/testing/src/sortTest.o \
+../src/obj/Debug/testing/src/subListTest.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
